c085fc014ef34bbd219b7b6ea11c728a37d344b8
Prietthi <92286803+Prietthi@users.noreply.github.com>
2021-10-23 18:56:11 +0100

    JavaFirstPrg from gitdesktop
    JavaFirstPrg from gitdesktop

---
+++ PreethiEclipseProjectSpace/src/TestPackage/JavaFirstPrg.java
@@ -0,0 +1,12 @@
+package TestPackage;
+
+public class JavaFirstPrg {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Welcome Java");
+System.out.println("Hello Java");	
+System.out.println("Programming in Java");	
+	}
+
+}
