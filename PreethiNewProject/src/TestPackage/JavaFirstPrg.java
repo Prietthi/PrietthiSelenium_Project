e4fc1a93f60472dcbc0b1cac78c9090dc42ae88e
Prietthi <92286803+Prietthi@users.noreply.github.com>
2021-10-24 06:03:09 +0100

    Updated new string statement in JavaFirstPrg class

--- PreethiNewProject/src/TestPackage/JavaFirstPrg.java
+++ PreethiNewProject/src/TestPackage/JavaFirstPrg.java
@@ -7,16 +7,6 @@ public class JavaFirstPrg {
 System.out.println("Welcome Java");
 System.out.println("Hello Java");	
 System.out.println("Programming in Java");	
-
-
-char[] ch={'j','a','v','a','t','p','o','i','n','t'};  
-String s=new String(ch);  
-
-String s1="javatpoint";  
-System.out.println(ch);
-System.out.println(s);
-System.out.println(s1);
-
 	}
 
 }
